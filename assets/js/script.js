d3ee6489de5a0c1192d8fa4c5692beaa2dca7165
chazzieloves <cassandra.olandersson@gmail.com>
2023-10-24 13:48:20 +0200

    add questions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const questionsData = [{
-        question: '1 - how are you?',
+        question: '1 - Which era is this iconic make-up from?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
@@ -8,7 +8,169 @@ const questionsData = [{
         correctOption: 'b'
     },
     {
-        question: '2 - how are you?',
+        question: '2 - Pearlescent from fish scales is used in cosmetics since the ancient egypt to get a ... ?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '3 - Before the 1920s, pale skin was a trend. Who changed this trend?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '4 - What is this product called? (used last in makeup routine)',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '5 - How much money is moved by the beauty industry per year?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '6 - What tones should she wear?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '7 - Which company was the first company producing hair-dye?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '8 - Eyebrows are one of the most important steps of a makeup routine. Why?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '9 - Which company created the first mascara in the 19th century?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '10 - What do you use this for?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '11 - Her lipstick is ... ?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '12 - Which alternative is incorrect, 1, 2 or 3, to create the perfect eyebrow-shape?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '13 - What do we call an eyeliner look that slims and enlongates our eyes, making them sharper?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '14 - Hairless armpits has not always been fashion, when did that change?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '15 - What is this facechart for?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '16 - Which country is the birthplace of the nailpolish?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '17 - Which eyeshadow-color would enhance this eye-color?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '18 - What is the name of this style?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '19 - What is this called?',
+        image: 'assets/...',
+        alt: 'the picture of smth',
+        options: [
+            'a', 'b', 'c'
+        ],
+        correctOption: 'b'
+    },
+    {
+        question: '20 - Where do you apply foundation to test if it´s the correct shade?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
