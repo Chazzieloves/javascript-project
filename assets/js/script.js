a69281a9ac49b9c641bdb161da22c7092fa41f71
chazzieloves <cassandra.olandersson@gmail.com>
2023-10-24 14:19:43 +0200

    add options + correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,180 +3,180 @@ const questionsData = [{
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            '1600-1700', '1700-1800', '1800-1900'
         ],
-        correctOption: 'b'
+        correctOption: '1700-1800'
     },
     {
         question: '2 - Pearlescent from fish scales is used in cosmetics since the ancient egypt to get a ... ?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Shimmery effect', 'Shiny effect', 'Glittery effect'
         ],
-        correctOption: 'b'
+        correctOption: 'Shimmery effect'
     },
     {
         question: '3 - Before the 1920s, pale skin was a trend. Who changed this trend?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Jeanne Lanvin', 'Elsa Schiaparelli', 'Coco Chanel'
         ],
-        correctOption: 'b'
+        correctOption: 'Coco Chanel'
     },
     {
         question: '4 - What is this product called? (used last in makeup routine)',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Primer', 'Setting Spray', 'Facemist'
         ],
-        correctOption: 'b'
+        correctOption: 'Setting Spray'
     },
     {
         question: '5 - How much money is moved by the beauty industry per year?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            '30 billion dollars', '20 billion dollars', '25 billions dollars'
         ],
-        correctOption: 'b'
+        correctOption: '20 billion dollars'
     },
     {
         question: '6 - What tones should she wear?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Cold', 'Neutral', 'Warm'
         ],
-        correctOption: 'b'
+        correctOption: 'Cold'
     },
     {
         question: '7 - Which company was the first company producing hair-dye?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'L´Oreal', 'Wella', 'Schwarzkopf'
         ],
-        correctOption: 'b'
+        correctOption: 'L´Oreal'
     },
     {
         question: '8 - Eyebrows are one of the most important steps of a makeup routine. Why?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'They need to match your hair', 'They appear to give you a facelift', 'They make your nose look straight'
         ],
-        correctOption: 'b'
+        correctOption: 'They appear to give you a facelift'
     },
     {
         question: '9 - Which company created the first mascara in the 19th century?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'L´Oreal', 'Max Factor', 'Maybelline'
         ],
-        correctOption: 'b'
+        correctOption: 'Maybelline'
     },
     {
         question: '10 - What do you use this for?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Nail cleaner', 'Face cleaner', 'Brush cleaner'
         ],
-        correctOption: 'b'
+        correctOption: 'Face cleaner'
     },
     {
         question: '11 - Her lipstick is ... ?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Matte', 'Shiny', 'Shimmery'
         ],
-        correctOption: 'b'
+        correctOption: 'Matte'
     },
     {
         question: '12 - Which alternative is incorrect, 1, 2 or 3, to create the perfect eyebrow-shape?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            '1', '2', '3'
         ],
-        correctOption: 'b'
+        correctOption: '3'
     },
     {
-        question: '13 - What do we call an eyeliner look that slims and enlongates our eyes, making them sharper?',
+        question: '13 - Which product is a must to achieve cateyes?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Eyeshadow', 'Kohl pencil', 'Eyeliner'
         ],
-        correctOption: 'b'
+        correctOption: 'Eyeliner'
     },
     {
         question: '14 - Hairless armpits has not always been fashion, when did that change?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            '1920', '1915', '1925'
         ],
-        correctOption: 'b'
+        correctOption: '1915'
     },
     {
         question: '15 - What is this facechart for?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Higlight/Contour', 'Blush/Rouge', 'Foundation/Concealer'
         ],
-        correctOption: 'b'
+        correctOption: 'Higlight/Contour'
     },
     {
         question: '16 - Which country is the birthplace of the nailpolish?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'France', 'Russia', 'Kina'
         ],
-        correctOption: 'b'
+        correctOption: 'Kina'
     },
     {
         question: '17 - Which eyeshadow-color would enhance this eye-color?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Green', 'Blue', 'Purple'
         ],
-        correctOption: 'b'
+        correctOption: 'Purple'
     },
     {
         question: '18 - What is the name of this style?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Vintage', 'Rockabilly', 'Classic'
         ],
-        correctOption: 'b'
+        correctOption: 'Vintage'
     },
     {
         question: '19 - What is this called?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            'Swatching', 'Mapping', 'Swapping'
         ],
-        correctOption: 'b'
+        correctOption: 'Swatching'
     },
     {
-        question: '20 - Where do you apply foundation to test if it´s the correct shade?',
+        question: '20 - Where do you apply foundation to test if it is the correct shade?',
         image: 'assets/...',
         alt: 'the picture of smth',
         options: [
-            'a', 'b', 'c'
+            '1', '2', '3'
         ],
-        correctOption: 'b'
+        correctOption: '2'
     },
 ]
 
