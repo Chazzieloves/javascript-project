9a9afc95b8cfe3cd9a20e4608d2f18649fd9be17
chazzieloves <cassandra.olandersson@gmail.com>
2023-10-24 15:26:25 +0200

    add images to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 const questionsData = [{
         question: '1 - Which era is this iconic make-up from?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img0.png',
+        alt: 'Marie Antoinette',
         options: [
             '1600-1700', '1700-1800', '1800-1900'
         ],
         correctOption: '1700-1800'
     },
     {
-        question: '2 - Pearlescent from fish scales is used in cosmetics since the ancient egypt to get a ... ?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '2 - Pearlescent from fish scales is used in cosmetics to get a ... ?',
+        image: 'assets/images/img1.png',
+        alt: 'Blue tang fish',
         options: [
             'Shimmery effect', 'Shiny effect', 'Glittery effect'
         ],
@@ -18,8 +18,8 @@ const questionsData = [{
     },
     {
         question: '3 - Before the 1920s, pale skin was a trend. Who changed this trend?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img2.png',
+        alt: 'Woman sunbathing',
         options: [
             'Jeanne Lanvin', 'Elsa Schiaparelli', 'Coco Chanel'
         ],
@@ -27,8 +27,8 @@ const questionsData = [{
     },
     {
         question: '4 - What is this product called? (used last in makeup routine)',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img3.png',
+        alt: 'MUA spraying model',
         options: [
             'Primer', 'Setting Spray', 'Facemist'
         ],
@@ -36,8 +36,8 @@ const questionsData = [{
     },
     {
         question: '5 - How much money is moved by the beauty industry per year?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img4.png',
+        alt: 'Money',
         options: [
             '30 billion dollars', '20 billion dollars', '25 billions dollars'
         ],
@@ -45,8 +45,8 @@ const questionsData = [{
     },
     {
         question: '6 - What tones should she wear?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img5.png',
+        alt: 'Blonde blue eyed girl',
         options: [
             'Cold', 'Neutral', 'Warm'
         ],
@@ -54,35 +54,35 @@ const questionsData = [{
     },
     {
         question: '7 - Which company was the first company producing hair-dye?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img6.png',
+        alt: 'Girl with red dyed hair',
         options: [
             'L´Oreal', 'Wella', 'Schwarzkopf'
         ],
         correctOption: 'L´Oreal'
     },
     {
-        question: '8 - Eyebrows are one of the most important steps of a makeup routine. Why?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '8 - Eyebrows can give you the visual effect of ... ?',
+        image: 'assets/images/img7.png',
+        alt: 'Brown eye with eyebrow',
         options: [
-            'They need to match your hair', 'They appear to give you a facelift', 'They make your nose look straight'
+            'a smaller face', 'a facelift', 'a slim nose'
         ],
-        correctOption: 'They appear to give you a facelift'
+        correctOption: 'a facelift'
     },
     {
         question: '9 - Which company created the first mascara in the 19th century?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img8.png',
+        alt: 'Girl putting on mascara',
         options: [
             'L´Oreal', 'Max Factor', 'Maybelline'
         ],
         correctOption: 'Maybelline'
     },
     {
-        question: '10 - What do you use this for?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '10 - What is this?',
+        image: 'assets/images/img9.png',
+        alt: 'Hand held machine with brush-head',
         options: [
             'Nail cleaner', 'Face cleaner', 'Brush cleaner'
         ],
@@ -90,17 +90,17 @@ const questionsData = [{
     },
     {
         question: '11 - Her lipstick is ... ?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img10.png',
+        alt: 'Girl with pink lipstick',
         options: [
-            'Matte', 'Shiny', 'Shimmery'
+            'matte', 'shiny', 'shimmery'
         ],
-        correctOption: 'Matte'
+        correctOption: 'matte'
     },
     {
-        question: '12 - Which alternative is incorrect, 1, 2 or 3, to create the perfect eyebrow-shape?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '12 - To get the perfect shape, which line is incorrect: 1, 2 or 3?',
+        image: 'assets/images/img11.png',
+        alt: 'Brow shape guide',
         options: [
             '1', '2', '3'
         ],
@@ -108,26 +108,26 @@ const questionsData = [{
     },
     {
         question: '13 - Which product is a must to achieve cateyes?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img12.png',
+        alt: 'Cat',
         options: [
             'Eyeshadow', 'Kohl pencil', 'Eyeliner'
         ],
         correctOption: 'Eyeliner'
     },
     {
-        question: '14 - Hairless armpits has not always been fashion, when did that change?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '14 - Hairless armpits became a trend in ... ?',
+        image: 'assets/images/img13.png',
+        alt: 'Woman with hairless armpits',
         options: [
             '1920', '1915', '1925'
         ],
         correctOption: '1915'
     },
     {
-        question: '15 - What is this facechart for?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '15 - This facechart is for ... ?',
+        image: 'assets/images/img14.png',
+        alt: 'Facechart',
         options: [
             'Higlight/Contour', 'Blush/Rouge', 'Foundation/Concealer'
         ],
@@ -135,26 +135,26 @@ const questionsData = [{
     },
     {
         question: '16 - Which country is the birthplace of the nailpolish?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img15.png',
+        alt: 'Red nails',
         options: [
-            'France', 'Russia', 'Kina'
+            'France', 'Russia', 'China'
         ],
-        correctOption: 'Kina'
+        correctOption: 'China'
     },
     {
         question: '17 - Which eyeshadow-color would enhance this eye-color?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img16.png',
+        alt: 'Green eye',
         options: [
             'Green', 'Blue', 'Purple'
         ],
         correctOption: 'Purple'
     },
     {
-        question: '18 - What is the name of this style?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '18 - What is this style called?',
+        image: 'assets/images/img17.png',
+        alt: 'Girl with bandana and red lipstick',
         options: [
             'Vintage', 'Rockabilly', 'Classic'
         ],
@@ -162,17 +162,17 @@ const questionsData = [{
     },
     {
         question: '19 - What is this called?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        image: 'assets/images/img18.png',
+        alt: 'Eyeshadow on arm',
         options: [
             'Swatching', 'Mapping', 'Swapping'
         ],
         correctOption: 'Swatching'
     },
     {
-        question: '20 - Where do you apply foundation to test if it is the correct shade?',
-        image: 'assets/...',
-        alt: 'the picture of smth',
+        question: '20 - The correct place to test foundation shade is: 1, 2 or 3?',
+        image: 'assets/images/img19.png',
+        alt: 'Girl with number 1-3 on her face',
         options: [
             '1', '2', '3'
         ],
